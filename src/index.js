162c6559ad4722fe358a631214deea1e3e7546cf
Mwapsam <mwapsam@gmail.com>
2022-01-17 14:33:11 +0200

    copied todo folder

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import './style.css';
+import {
+  createTodo,
+  showEditInput,
+  toggleComplete,
+  updateTodo,
+  loadTodo,
+} from './modules/crud.js';
+
+const list = document.getElementById('todos-list');
+const addInput = document.getElementById('todo-input');
+const addBtn = document.getElementById('rotate');
+
+list.addEventListener('click', (event) => {
+  event.stopPropagation();
+  switch (event.target.tagName) {
+    case 'P':
+      showEditInput(event.target);
+      break;
+
+    default:
+  }
+});
+
+list.addEventListener('change', (event) => {
+  if (event.target.tagName === 'INPUT' && event.target.type === 'checkbox') {
+    toggleComplete(event.target);
+  }
+});
+
+list.addEventListener('keypress', (event) => {
+  if (
+    event.target.tagName === 'INPUT'
+    && event.target.type === 'text'
+    && event.key === 'Enter'
+  ) {
+    updateTodo();
+  }
+});
+
+document.addEventListener('click', updateTodo());
+
+addBtn.addEventListener('click', (event) => {
+  event.stopPropagation();
+  createTodo();
+});
+
+addInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    createTodo();
+  }
+});
+
+window.onload = loadTodo();
